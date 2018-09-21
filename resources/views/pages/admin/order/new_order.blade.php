4896041b5097eff54b8b2e080b77bd1cb7400598
Ajay Kumar Yadav <ajaykucse11@hotmail.com>
2018-09-21 13:52:34 +0600

    first commit

---
+++ resources/views/pages/admin/order/new_order.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin.master')
+
+@section('title','new order')
+@section('after-style')
+
+@endsection
+
+@section('content')
+
+
+    <div class="form-control">
+        <h1> New Order</h1>
+    </div>
+
+    <!-- start: Content -->
+
+    </div><!--/.fluid-container-->
+
+    <!-- end: Content -->
+
+@endsection
+
+
+<script src="{{asset('admin/assets/js/vendor/jquery-2.1.4.min.js')}}"></script>
+<script src="{{asset('admin/assets/js/popper.min.js')}}"></script>
+<script src="{{asset('admin/assets/js/plugins.js')}}"></script>
+<script src="{{asset('admin/assets/js/main.js')}}"></script>
+
+
+@section('script')
+@endsection
